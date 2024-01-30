cb8050f9897a1f8d0f24a17bc2ee5fe6597c44b9
tw31120071857 <tw31122007@googlemail.com>
2024-01-30 22:02:29 +1100

    1

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: "https://aesthetic-liger-690214.netlify.app,https://movie-web-proxy.desmoscalc.me,https://movie-web-proxy.alegithomeworksite.com.au",
+  VITE_CORS_PROXY_URL: "https://aesthetic-liger-690214.netlify.app",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Y2FhZjVmNDU5OThlNDUwZTc4M2JjNjNiOWQ0NmM4OCIsInN1YiI6IjY0ZTZlNmJhYzYxM2NlMDEyY2M0NWY3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kNVfK69J_wLFxtDZDc3BBkiKDZmdLCf9RrVdKWuuu4k",
