26d876d66eb725daed13802325ae0e3da1abda6f
tw31122007 <tw31122007@googlemail.com>
2024-02-02 09:11:27 +1100

    1

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: "https://movie-web-proxy.desmoscalc.me",
+  VITE_CORS_PROXY_URL: "https://movie-web-proxy.notapr0xy.com.au",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Y2FhZjVmNDU5OThlNDUwZTc4M2JjNjNiOWQ0NmM4OCIsInN1YiI6IjY0ZTZlNmJhYzYxM2NlMDEyY2M0NWY3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kNVfK69J_wLFxtDZDc3BBkiKDZmdLCf9RrVdKWuuu4k",
